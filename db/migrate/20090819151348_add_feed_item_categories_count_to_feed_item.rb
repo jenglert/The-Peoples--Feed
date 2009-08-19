2d2a6e1e543f5ffb823eaab877ea5c744b59d27c
James Englert <englert.james@gmail.com>
2009-08-19 13:14:11 -0400

    Updating the count column initially.

--- db/migrate/20090819151348_add_feed_item_categories_count_to_feed_item.rb
+++ db/migrate/20090819151348_add_feed_item_categories_count_to_feed_item.rb
@@ -1,6 +1,12 @@
 class AddFeedItemCategoriesCountToFeedItem < ActiveRecord::Migration
   def self.up
     add_column :feed_items, :feed_item_categories_count, :integer
+    
+    FeedItem.reset_column_information
+    
+    FeedItem.find(:all).each do |f|
+      FeedItem.update_counters f.id, :feed_item_categories_count => f.categories.length
+    end
   end
 
   def self.down
