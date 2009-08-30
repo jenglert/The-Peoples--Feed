3b3bd2d69b49fed62e17a795aa7221fe2e1d815d
James Englert <englert.james@gmail.com>
2009-08-30 14:04:54 -0400

    Changed gem version.  The production server can only use this gem version.

--- lib/dryopteris.rb
+++ lib/dryopteris.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'rubygems'
-gem     'nokogiri', '>=1.2.4'
+gem     'nokogiri', '>=1.1.0'
 require 'nokogiri'
 
 require "dryopteris/whitelist"
