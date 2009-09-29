94cac54b981fea5d630c26c3b7b3450053db9e6e
James Englert <englert.james@gmail.com>
2009-09-28 23:16:40 -0400

    Changing from address

--- app/models/consumer_mailer.rb
+++ app/models/consumer_mailer.rb
@@ -2,7 +2,7 @@ class ConsumerMailer < ActionMailer::Base
 
   def email_a_friend(email_a_friend_settings)
     recipients email_a_friend_settings.recipient_email_address
-    from email_a_friend_settings.sender_email_address
+    from "webmaster@thepeoplesfeed.com"
     subject "#{email_a_friend_settings.title} - The People's Feed"
     body :email_a_friend_settings => email_a_friend_settings
   end  
