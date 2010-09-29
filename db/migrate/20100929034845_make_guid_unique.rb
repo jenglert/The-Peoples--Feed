8274942314d9aa63bffc921102a3fcae4a22bd77
Jim Englert <jimenglert@jim-englerts-macbook.local>
2010-09-28 23:59:37 -0400

    Enhancing incremental

--- db/migrate/20100929034845_make_guid_unique.rb
+++ db/migrate/20100929034845_make_guid_unique.rb
@@ -4,6 +4,7 @@ class MakeGuidUnique < ActiveRecord::Migration
     execute "create index feed_item_guid_idx on feed_items(guid)"
     
     FeedItem.all.each do |fi|
+      puts fi.id if fi.id % 1000 == 0
       FeedItem.find_all_by_guid(fi.guid).each do |found_feed_item| 
         if found_feed_item.id != fi.id
           found_feed_item.destroy
