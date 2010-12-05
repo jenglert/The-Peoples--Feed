82a85b59b8e82dcbb8923d4bd524dd0658ed3982
Jim Englert <englert.james@gmail.com>
2010-12-04 21:25:15 -0500

    cleaning up script

--- script/clearCaches.sh
+++ script/clearCaches.sh
@@ -1,8 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 rm ~/Documents/thepeoplesfeed/public/sitemap.xml
 rm -rf ~/Documents/thepeoplesfeed/tmp/cache/*
-wget --spider http://localhost:9000/sitemap.xml
-wget --spider http://localhost:9000/contribute
-wget --spider http://localhost:9000/feed/new
-wget --spider http://localhost:9000/category/2291
-wget --spider http://localhost:9000/feed/25
